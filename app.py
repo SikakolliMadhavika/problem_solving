7fb63b7b7d9c3963033fab47fd8afcaef9bc7cfa
SikakolliMadhavika-github <140150457+SikakolliMadhavika@users.noreply.github.com>
2025-03-08 18:11:33 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
-import math
-def apptitudeValues(num1, num2):
-    value = num1/num2
-    print(value)
-    print(math.floor(value))
-    print(math.ceil(value))
-    print(value - math.floor(value))
+# import math
+# def apptitude_using_math(num1, num2):
+#     value = num1/num2
+#     print(value)
+#     print(math.floor(value))
+#     print(math.ceil(value))
+#     print(value - math.floor(value))
 
-apptitudeValues(13,5)
-apptitudeValues(123,10)
+# apptitude_using_math(13,5)
+
+def apptitude_without_math(num1, num2):
+    value = num1/num2
+    floorvalue = num1//num2
+    decimal = value - floorvalue
+    print("The quotient value is",value)
+    print("The decimal value is",decimal)
+    if(decimal>0.5):
+        print("The nearest or considered integer is",floorvalue + 1 )
+    else:
+        print("The nearest or considered integer is",floorvalue )
+    print("The smallest integer value is",floorvalue)
+    print("The biggest integer value is",floorvalue + 1)
+    
+
+apptitude_without_math(131,5)
